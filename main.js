2a35b960926fe3a8a2da732395be9986f1850b4a
Uli Orduno <uli.orduno@codeimmersives.com>
2019-02-26 15:14:26 -0500

    Javascript Project

--- main.js
+++ main.js
@@ -1,13 +1,59 @@
 window.onload = init;
+window.onload=typeText;
 
+/*sets the variables for String and Index*/
+var i = 0;
+var txt = 'Enter Your Text Here';
+var speed = 50;
+/*function for type effect*/
+function typeText() {
+  if (i < txt.length) {
+    document.getElementById("letters").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeText, speed);
+  }
+}
+/*text box*/
 function init() {
     document.querySelector('#input')
-        .addEventListener('keydown', handleTyping);
+        .addEventListener('keyup', handleTyping);
 }
 
 function handleTyping(event) {
-    let userInput = event.target.value;
-    let answer = userInput;
+    /*defines input*/
+    let userInput = event.target.value.toLowerCase();
+    /* makes input to lower-case and creates arrays*/
+    /*identifies index at zero*/
+    let answer = "";
+    let i = 0;
     
-    document.querySelector('#changeme').innerText = answer;
+    /*Loop Capitalizes the first Letter */
+    while (i < userInput.length){
+        if (i === 0 || userInput[i-1]===' '){
+            answer = answer + userInput[i].toUpperCase();
+        } else { 
+            answer = answer + userInput[i];
+
+        }
+       i++; 
+    }
+    
+    document.querySelector('#blurb').innerText = answer;
 }
+
+
+
+function myMove() {
+    let elem = document.getElementById("animate");   
+    let pos = 0;
+    let id = setInterval(frame, -20);
+    function frame() {
+      if (pos == 500) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.left= pos + "px"; 
+        elem.style.right= pos + "px"; 
+      }
+    }
+  }
